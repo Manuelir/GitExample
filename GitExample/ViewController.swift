d3bc5d715d9cc086c1b849f5ddb694ca75bbb40e
Manuel Ibáñez Rodríguez <mibanez@atsistemas.com>
2019-11-21 20:26:01 +0100

    Initial Commit

---
+++ GitExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitExample
+//
+//  Created by Manuel Ibáñez Rodríguez on 21/11/2019.
+//  Copyright © 2019 Manuel Ibáñez Rodríguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
