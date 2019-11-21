0bdb05d095458f1670ef1f47c22bb62fdf4ae42e
Manuel Ibáñez Rodríguez <mibanez@atsistemas.com>
2019-11-21 20:39:04 +0100

    Third commit

--- GitExample/AppDelegate.swift
+++ GitExample/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         print("HA arrancado la aplicación")
+        print("CAMBIO EN DEVELOP")
         return true
     }
 
