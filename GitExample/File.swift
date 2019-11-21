8de1e795eeee89d165a12d64108fa40c585f0f5d
Manuel Ibáñez Rodríguez <mibanez@atsistemas.com>
2019-11-21 20:33:31 +0100

    Second commit

---
+++ GitExample/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  GitExample
+//
+//  Created by Manuel Ibáñez Rodríguez on 21/11/2019.
+//  Copyright © 2019 Manuel Ibáñez Rodríguez. All rights reserved.
+//
+
+import Foundation
